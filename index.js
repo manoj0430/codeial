e37d56f434ec5093a4ec625c02fc48d4a7ce9f0c
manasa lingabathula <manojl0430@gmail.com>
2023-07-14 00:01:04 +0530

    Setup static file access for layout and pages

--- index.js
+++ index.js
@@ -2,13 +2,24 @@ const express = require('express');
 const app=express();
 
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
 
+//using static files
+
+app.use(express.static('./assets'));
+
+//Using Layouts
+app.use(expressLayouts);
+
+//extract style and scripts from subsrcipts
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 //use express router
 
 app.use('/', require('./routes'));
 
 //setting up view engine
-app.set('viewengine', 'ejs');
+app.set('view engine', 'ejs');
 app.set('views', './views');
 
 
