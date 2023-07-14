ed4445ba29247236416c27ad22dfe30477746db0
manasa lingabathula <manojl0430@gmail.com>
2023-07-14 10:01:59 +0530

    Setup mongodb server

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const app=express();
+const app= express();
 
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
-
+const db = require('./config/mongoose');
 //using static files
 
 app.use(express.static('./assets'));
