db435a67b18bb57d3db53aaab5d1bba1bed6c0e2
manasa lingabathula <manojl0430@gmail.com>
2023-07-14 20:22:15 +0530

    Create Signup Authenticatio

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require('../modals/user');
+
 module.exports.profile = function(req,res){
     return res.render('user_profile', {
         title: 'User Profile'
@@ -6,4 +8,71 @@ module.exports.profile = function(req,res){
 
 module.exports.name = function(req,res){
     return res.end('<h2> Name is rendered</h2>');
+}
+
+//render sign-up 
+
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up', {
+        title: "Sign Up"
+    });
+};
+
+// Render Sign-in
+
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in', {
+        title: "Sign In"
+    });
+};
+
+// render Sign up form
+
+module.exports.create = function(req,res){
+    //if password and confirm password doesnot match
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+    
+    // User.findOne({email: req.body.email}, function(err, user){
+    //     if(err){
+    //         console.log('Error in finding user in signing up');
+    //         return;
+    //     }
+        
+    //     if(!user){
+    //         User.create(req.body, function(err,user){
+    //             if(err){
+    //                 console.log('Error in creating user');
+    //                 return;
+    //             }
+    //             return res.redirect('/users/sign-in');
+    //         })
+    //     }else{
+    //         return res.redirect('back');
+    //     }
+    // })
+
+    User.findOne({ email: req.body.email })
+  .then((user) => {
+    if (!user) {
+      return User.create(req.body);
+    } else {
+      throw new Error('User already exists');
+    }
+  })
+  .then((user) => {
+    return res.redirect('/users/sign-in');
+  })
+  .catch((err) => {
+    console.log('Error:', err.message);
+    return res.redirect('back');
+  }); 
+
+    
+}
+
+//sign in and create a session for user
+module.exports.createSession=function(req,res){
+
 }
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,12 @@ const userController = require('../controllers/users_controller');
 router.get('/profile',userController.profile);
 router.get('/user',userController.name);
 
+//Routing to Sign Up and Sign In
+
+router.get('/sign-up',userController.signUp);
+router.get('/sign-in',userController.signIn);
+
+//create a route for Create user
+router.post('/create', userController.create);
+
 module.exports=router;
